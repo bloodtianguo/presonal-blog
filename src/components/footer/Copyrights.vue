54b9ef7759a28911cee05f870fa2c9eea26333bd
bloodtianguo <2514003467@qq.com>
2021-03-25 16:16:31 +0800

    初步配置文件

---
+++ src/components/footer/Copyrights.vue
@@ -0,0 +1,18 @@
+<template>
+    <a :style="{color: color, 'font-size': size + 'em'}" href="https://github.com/manerfan/vuesume/blob/master/LICENSE" target="_blank">
+        ©2020 林舍|Maner·Fan All rights reserved
+    </a>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from 'vue-property-decorator';
+
+    @Component({
+        props: {
+            color: String,
+            size: Number,
+        },
+    })
+    export default class Copyrights extends Vue {
+    }
+</script>
